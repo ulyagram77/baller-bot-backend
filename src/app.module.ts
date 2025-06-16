4d90e7b4b66545574750adcdb3009aca2754bbf6
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-06-16 16:33:49 +0300

    chore: setuped prisma ORM + sqlite DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './config/database/database.module';
 
-@Module({
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({ imports: [DatabaseModule] })
 export class AppModule {}
---
+++ src/config/core.config.ts
@@ -0,0 +1,3 @@
+import * as path from 'node:path';
+
+export const DATA_FOLDER_PATH = path.resolve(__dirname, '../../data');
