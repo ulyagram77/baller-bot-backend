815f7125ffc1b57f8a7d6782900a702a475b51fb
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-06-23 22:03:42 +0300

    chore: small folder changes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { swaggerDoc } from './config/swagger';
+import { swaggerDoc } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
